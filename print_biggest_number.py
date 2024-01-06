554eb814557c92e6db05d4a5d614ce81c2e7be24
ccesspearl <princesspearldelarosa05@gmail.com>
2024-01-06 23:15:38 +0800

    turning the previous code into def function for tkinter

--- print_biggest_number.py
+++ print_biggest_number.py
@@ -3,30 +3,52 @@
 # Pseudocode
 
 # ask user to input 3 numbers
-print("We will ask you to input 3 numbers. In order to find the biggest number, you will only input numbers and not letters. Do not input same numbers.")
-print ()
+#print("We will ask you to input 3 numbers. In order to find the biggest number, you will only input numbers and not letters. Do not input same numbers.")
+#print ()
 
 # ask user to input 1st number
-first_number = float(input("Input first number: "))
+#first_number = float(input("Input first number: "))
 
 # ask user to input 2nd number
-second_number = float(input("Input second number: ")) 
+#second_number = float(input("Input second number: ")) 
 
 # ask user to input 3rd number
-third_number = int(input("Input third number: "))
+#third_number = int(input("Input third number: "))
 
 # check if the first number is bigger
-if first_number > second_number and first_number > third_number:
-    biggest = first_number
+#if first_number > second_number and first_number > third_number:
+    #biggest = first_number
 
 # check if the second number is bigger
-elif second_number > first_number and second_number > third_number:
-    biggest = second_number
+#elif second_number > first_number and second_number > third_number:
+    #biggest = second_number
 
 # check if the third number is bigger
-else:
-    biggest = third_number 
+#else:
+    #biggest = third_number 
 
 # print the biggest number
-print()
-print(biggest, "is the biggest number")
+#print()
+#print(biggest, "is the biggest number")
+
+
+
+
+# using Tkinter 
+import tkinter as tk
+
+# Turning the previous code into def function 
+def find_biggest():
+    first_number = float(entry1.get())
+    second_number = float(entry2.get())
+    third_number = float(entry3.get())
+
+    if first_number > second_number and first_number > third_number:
+        biggest = first_number
+    elif second_number > first_number and second_number > third_number:
+        biggest = second_number
+    else:
+        biggest = third_number
+
+    result_label.config(result_label.config(text=f"{biggest} is the biggest number"))
+
