600c2b92ef0f7f48d99f46854a029eba5b5db684
ccesspearl <princesspearldelarosa05@gmail.com>
2024-01-06 18:16:41 +0800

    committing the pseudocode again

---
+++ print_biggest_number.py
@@ -0,0 +1,11 @@
+# Ask user to input 3 numbers. Find and print the biggest number using only if-else statement
+
+# Pseudocode
+# ask user to input 3 numbers
+# ask user to input 1st number
+# ask user to input 2nd number
+# ask user to input 3rd number
+# check if the first number is bigger
+# check if the second number is bigger
+# check if the third number is bigger
+# print the biggest number
