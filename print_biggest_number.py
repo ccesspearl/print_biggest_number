988b4c2d1975c39daa098ddc2d494cdc66d25b74
ccesspearl <princesspearldelarosa05@gmail.com>
2024-01-06 18:24:30 +0800

    asking user to input 1st number

--- print_biggest_number.py
+++ print_biggest_number.py
@@ -1,10 +1,14 @@
 # Ask user to input 3 numbers. Find and print the biggest number using only if-else statement
 
 # Pseudocode
+
 # ask user to input 3 numbers
 print("We will ask you to input 3 numbers. In order to find the biggest number, you will only input numbers and not letters. Do not input same numbers.")
+print ()
 
 # ask user to input 1st number
+first_number = float(input("Input first number: "))
+
 # ask user to input 2nd number
 # ask user to input 3rd number
 # check if the first number is bigger
